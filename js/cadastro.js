792f22dbe08066111d8daae228256c36ac0a72b7
Martins <145706422+DevCarlosMartins@users.noreply.github.com>
2025-05-27 21:40:35 -0300

    loing1.2

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,36 +1,39 @@
-const botao = document.getElementById("btnCadastrar"); // Corrigido para id
-const listaUsuariosCadastrados = [];
+// fazer o cadastro
+const botao = document.getElementById('btnCadastrar');
+//const listaUsuariosCadastrados = [];
 
-// Cadastrar Usuário
-botao.addEventListener('click', function(){
-  const objUsuario = {
-    usuario: document.getElementById("usuarios").value, // Corrigido para id
-    senha: document.getElementById("senha").value       // Corrigido para id
-  };
-  listaUsuariosCadastrados.push(objUsuario);
-  const listaJson = JSON.stringify(listaUsuariosCadastrados);
-  localStorage.setItem("usuarios", listaJson);
-  listar();
-});
+// cadastrar
+botao.addEventListener('click', function (){
+    const listaUsuariosCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+    const objUsuario = {
+        usuario: document.getElementById('usuario').value,
+        senha: document.getElementById('senha').value
+    };
+    listaUsuariosCadastrados.push(objUsuario);
+    const listaJson = JSON.stringify(listaUsuariosCadastrados);
+    localStorage.setItem('usuarios', listaJson);
+    listar();
+}
 
-// Listar Usuários
+);
+
+//listar
 function listar(){
-  const listaUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-  let tabela = document.getElementById('listaUsuariosCadastrados');
-  tabela.innerHTML = "";
-
-  listaUsuarios.forEach((user, index ) =>{
-    let linha = document.createElement('tr');
-    linha.innerHTML = `
-      <td>${user.usuario}</td>
-      <td>${user.senha}</td>
-      <td>
-        <button>Editar</button>
-        <button>Remover</button>
-      </td>
-    `;
-    tabela.appendChild(linha);
-  });
+    const listaUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    let tabela = document.getElementById('listaUsuariosCadastrados');
+    tabela.innerHTML = "";
+    listaUsuarios.forEach((objeto, index) => {
+        let linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${objeto.usuario}</td>
+            <td>${objeto.senha}</td>
+            <td>
+                <button onclick="editarUsuario()">Editar</button>
+                <button onclick="removerUsuario()">Remover</button>
+            </td>
+        `;
+        tabela.appendChild(linha);
+    });
 }
 
 listar();
